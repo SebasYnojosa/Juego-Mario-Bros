fd21b027eeca6cf43d02ff6f6e32b2046c6a93ce
angel5112 <aepatino.360@gmail.com>
2023-07-09 14:14:57 -0400

    Agregado cambios en el sprite de peach.

--- src/main/Juego.java
+++ src/main/Juego.java
@@ -32,7 +32,7 @@ public class Juego implements Runnable {
 
     public Juego() {
         manejaNiveles = new ManejaNiveles(this);
-        jugador = new Jugador(100, 100, ANCHURA_JUGADOR, ALTURA_JUGADOR, ImagenURL.MARIO_SPRITESHEET);
+        jugador = new Jugador(100, 100, ANCHURA_JUGADOR, ALTURA_JUGADOR, ImagenURL.PEACH_SPRITESHEET);
         jugador.cargarInfoNivel(manejaNiveles.getNivelActual().getInfoNivel());
 
         panel = new Panel(this);
